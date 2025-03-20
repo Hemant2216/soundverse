35d5ac44eca1f43651efd07f835b4044817f22ba
Hemant2216 <88074414+Hemant2216@users.noreply.github.com>
2025-03-20 19:18:41 +0530

    Update artists.js

--- frontend/src/app/api/artists.js
+++ frontend/src/app/api/artists.js
@@ -1,5 +1,5 @@
 export async function getArtists() {
-  const response = await fetch("http://localhost:8000/artists");
+  const response = await fetch("https://soundverse-t42y.onrender.com/artists");
   const data = await response.json();
   console.log("API Response:", data); // Debugging
   return Array.isArray(data) ? data : [];
