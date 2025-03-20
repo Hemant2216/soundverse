fe16e4500678864f6b89ceaacb12a769adb021fd
Hemant2216 <88074414+Hemant2216@users.noreply.github.com>
2025-03-20 12:55:32 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/soundverse"
+DATABASE_URL = "postgresql://root:Lz441af5qjA1g9biLkavuWE9Serg8Rdo@dpg-cvds4lpc1ekc73e9alo0-a.oregon-postgres.render.com/soundverse"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
